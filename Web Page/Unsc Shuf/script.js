d0a0f081823e1254c3e3a7e65d89923019dda6c2
DexxterGWM <99261879+DexxterGWM@users.noreply.github.com>
2022-10-14 11:30:43 -0300

    script.js File

---
+++ Web Page/Unsc Shuf/script.js
@@ -0,0 +1,27 @@
+const boxesContainer = document.getElementById('boxes')
+const btn = document.getElementById('btn')
+
+btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
+
+function createBoxes() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      const box = document.createElement('div')
+      box.classList.add('box')
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+      boxesContainer.appendChild(box)
+    }
+  }
+}
+
+createBoxes();
+
+btn.addEventListener('click', function handleClick() {
+  const initialText = 'Shuffle';
+
+  if (btn.textContent.toLowerCase().includes(initialText.toLowerCase())) {
+    btn.innerHTML = 'Unscramble';
+  } else {
+    btn.innerHTML = initialText;
+  }
+})
